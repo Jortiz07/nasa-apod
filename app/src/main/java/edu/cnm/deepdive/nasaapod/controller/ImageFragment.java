74f492999db6d73ad2e1e3c8588ee047ee94df3a
Jose Ortiz <jose.ortiz@live.com>
2020-01-31 09:47:28 -0700

    Added info icon to options bar, and the APoD title,description, copyright information, and date to the popup dialog.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
@@ -2,8 +2,10 @@ package edu.cnm.deepdive.nasaapod.controller;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -18,7 +20,6 @@ import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import edu.cnm.deepdive.android.DateTimePickerFragment;
 import edu.cnm.deepdive.android.DateTimePickerFragment.Mode;
-import edu.cnm.deepdive.android.DateTimePickerFragment.OnChangeListener;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.model.Apod;
 import edu.cnm.deepdive.nasaapod.viewmodel.MainViewModel;
@@ -34,6 +35,13 @@ public class ImageFragment extends Fragment {
   private ProgressBar loading;
   private FloatingActionButton calendar;
   private Apod apod;
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+    setRetainInstance(true);
+  }
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater,
@@ -66,6 +74,36 @@ public class ImageFragment extends Fragment {
       toast.show();
 
     });
+  }
+
+  @Override
+  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.options, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.info:
+        if (apod != null) {
+          InfoFragment fragment = new InfoFragment();
+          Bundle args = new Bundle();
+          args.putString(InfoFragment.TITLE_KEY, apod.getTitle());
+          args.putString(InfoFragment.DESCRIPTION_KEY, apod.getDescription());
+          args.putString(InfoFragment.COPYRIGHT_KEY, apod.getCopyright());
+          args.putSerializable(InfoFragment.DATE_KEY, apod.getDate());
+          fragment.setArguments(args);
+          fragment.show(getChildFragmentManager(), fragment.getClass().getName());
+        }
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+
+    }
+    return handled;
+
   }
 
   private void setupWebView(View root) {
